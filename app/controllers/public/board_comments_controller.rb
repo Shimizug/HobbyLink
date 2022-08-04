bbc0d15257e9fec0c2473c312bec4eb08e1379b4
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-08-04 07:23:05 +0000

    public側のコントローラの追加

---
+++ app/controllers/public/board_comments_controller.rb
@@ -0,0 +1,2 @@
+class Public::BoardCommentsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'relationships/followings'
+    get 'relationships/followers'
+  end
+  namespace :public do
+    get 'boards/index'
+    get 'boards/show'
+    get 'boards/edit'
+    get 'boards/new'
+  end
+  namespace :public do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+    get 'posts/new'
+  end
+  namespace :public do
+    get 'members/index'
+    get 'members/show'
+    get 'members/edit'
+    get 'members/unsubscribe'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
 # 会員用
 devise_for :members,skip: [:passwords], controllers: {
   registrations: "public/registrations",
