ef11309b55fe78081cc8d2bb3d0611240829a77f
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-09-01 06:37:22 +0000

    ランダム機能の修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,19 +4,19 @@ class Public::HomesController < ApplicationController
     #最新の投稿4件を取得する
     @new_posts = Post.all.last(4)
     #ランダムでいすれかの投稿を取得する
-    @random_posts = Post.order("RANDOM()").limit(4)
+    # @random_posts = Post.order("RANDOM()").limit(4)
     
     # mysqlでの実行時
-    # @random_posts = Post.order("RAND()").limit(4)
+    @random_posts = Post.order("RAND()").limit(4)
   end
 
   def about
     #最新の投稿4件を表示する
     @new_posts = Post.all.last(4)
     #ランダムでいすれかの投稿を取得する
-    @random_posts = Post.order("RANDOM()").limit(4)
+    # @random_posts = Post.order("RANDOM()").limit(4)
     
      # mysqlでの実行時
-    # @random_posts = Post.order("RAND()").limit(4)
+    @random_posts = Post.order("RAND()").limit(4)
   end
 end
