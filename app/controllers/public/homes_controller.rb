7f18abce56c68d7865659da7b9673b8987de82e4
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-09-01 11:04:30 +0000

    homesコントーラに分岐作成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,19 +4,23 @@ class Public::HomesController < ApplicationController
     #最新の投稿4件を取得する
     @new_posts = Post.all.last(4)
     #ランダムでいすれかの投稿を取得する
-    # @random_posts = Post.order("RANDOM()").limit(4)
-    
+    if ENV["RAILS_ENV"] == "development" || ENV["RAILS_ENV"] == "test"
+      @random_posts = Post.order("RANDOM()").limit(4)
+    else
     # mysqlでの実行時
-    @random_posts = Post.order("RAND()").limit(4)
+      @random_posts = Post.order("RAND()").limit(4)
+    end
   end
 
   def about
     #最新の投稿4件を表示する
     @new_posts = Post.all.last(4)
-    #ランダムでいすれかの投稿を取得する
-    # @random_posts = Post.order("RANDOM()").limit(4)
-    
-     # mysqlでの実行時
-    @random_posts = Post.order("RAND()").limit(4)
+   #ランダムでいすれかの投稿を取得する
+    if ENV["RAILS_ENV"] == "development" || ENV["RAILS_ENV"] == "test"
+      @random_posts = Post.order("RANDOM()").limit(4)
+    else
+    # mysqlでの実行時
+      @random_posts = Post.order("RAND()").limit(4)
+    end
   end
 end
