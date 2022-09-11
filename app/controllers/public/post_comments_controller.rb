ebbe71faaae8e299f1b52e815f2d6fd0b18e3a66
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-09-11 07:04:30 +0000

    public/post_commentの非同期化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,15 +2,18 @@ class Public::PostCommentsController < ApplicationController
   before_action :authenticate_member!
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = current_member.post_comments.new(post_comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
+    @post_comments = @post.post_comments.page(params[:page])
     # redirect_to request.referer
   end
 
   def destroy
     PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    @post = Post.find(params[:post_id])
+    @post_comments = @post.post_comments.page(params[:page])
     # redirect_to request.referer
   end
 
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -27,7 +27,6 @@ class Public::PostsController < ApplicationController
       end
       redirect_to post_path(@post), notice: "投稿が正常に作成されました。"
     else
-      @post = Post.new
       @genres = Genre.all
       render :new
     end
